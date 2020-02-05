87bba2a0f932065da7644648a84010ba3bbb98c6
Priit Lätt <priit@nevercode.io>
2020-02-05 14:51:41 +0200

    Add iOS scheme 'dev'

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/priit/Library/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/priit/AndroidStudioProjects/capybara"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/priit/AndroidStudioProjects/capybara/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/priit/Library/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/priit/Library/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.2"
 export "FLUTTER_BUILD_NUMBER=3"
+export "TRACK_WIDGET_CREATION=true"
